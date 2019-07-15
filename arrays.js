eb5fe3a390de202a650259311f70e70c06ee9dfc
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-07-15 04:18:57 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element){
-  var array = ['San Diego', 'Chciago', "foo"];
+  var array = ["foo"];
   array.unshift("Miami");
   return array;
 }
