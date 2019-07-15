df25e38da9e3a807d67ac43e088ad5a05d6a1d82
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-07-15 04:16:59 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,10 @@
+
+function addElementToBeginningOfArray(array, element){
+  var array = ['San Diego', 'Chciago'];
+  array.unshift("Miami");
+  return array;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+
+}
