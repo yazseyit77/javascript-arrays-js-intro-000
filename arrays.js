05d36a066e6214c188fb3805701f22058e1a3677
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-07-15 04:49:51 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -29,7 +29,7 @@ function accessElementInArray(array, index){
   index = array[3];
   return index;
 }
-function removeElementFromBeginningOfArray(array){
+function destructivelyAddElementToEndOfArray(array){
   var array = [1, 2 ,3];
   array.shift();
   return array;
