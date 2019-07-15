a46f3e15b896711c3b5bf647793645e7d4f7595a
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-07-15 05:37:17 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -8,7 +8,7 @@ function addElementToBeginningOfArray(array, element){
 
 function destructivelyAddElementToBeginningOfArray(array, element){
   var array = [1];
-  array.unshift("foo");
+  array.unshift('foo');
   return array;
 }
 
